72f6780c6a281985a915af5cbe652f87d73d3a9b
CyberTML <cybertml@outlook.com>
2022-08-18 17:53:24 -0400

    Decorated Headers

---
+++ CodeTemplates/Headers.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Templates
+{
+    internal class Generators
+    {
+        static void Main(string[] args)
+        {
+            #region HEADER TEMP
+
+            Console.WriteLine("\n\n==================================");
+            Console.WriteLine(":::::::: MENU NAME HERE!! ::::::::");
+            Console.WriteLine("==================================\n");
+
+
+                     
+            Console.WriteLine("\n\n♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦");
+            Console.WriteLine("|≡≡≡≡≡≡≡≡ MENU NAME HERE ≡≡≡≡≡≡≡≡|");
+            Console.WriteLine("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n");
+
+
+
+            Console.WriteLine("\n\n╔════════════════════════════════╗");
+            Console.WriteLine("║›››››››› MENU ITEM HERE ‹‹‹‹‹‹‹‹║");
+            Console.WriteLine("╚════════════════════════════════╝");
+
+
+
+            Console.WriteLine("\n\n╔════════════════════════════════╗");
+            Console.WriteLine("║ºººººººº MENU ITEM HERE ºººººººº║");
+            Console.WriteLine("╚════════════════════════════════╝");
+
+
+            
+            
+            Console.WriteLine("\n\n╔════════════════════════════════╗");
+            Console.WriteLine("║×××××××× MENU ITEM HERE ××××××××║");
+            Console.WriteLine("╚════════════════════════════════╝");
+
+
+
+            Console.WriteLine("\n\n┌════════════════════════════════┐");
+            Console.WriteLine("║»»»»»»»» MENU ITEM HERE ««««««««║");
+            Console.WriteLine("└════════════════════════════════┘");
+
+
+
+            Console.WriteLine("\n\n┌════════════════════════════════┐");
+            Console.WriteLine("│♦♣♥♠♦♣♥♠ MENU ITEM HERE ♦♣♥♠♦♣♥♠│");
+            Console.WriteLine("└════════════════════════════════┘");
+
+
+
+            Console.WriteLine("\n\n┌────────────────────────────────┐");
+            Console.WriteLine("│¥¥¥¥¥¥¥¥ MENU ITEM HERE ¥¥¥¥¥¥¥¥│");
+            Console.WriteLine("└────────────────────────────────┘");
+
+
+
+            Console.WriteLine("\n\n┌────────────────────────────────┐");
+            Console.WriteLine("│         MENU ITEM HERE         │");
+            Console.WriteLine("└────────────────────────────────┘");
+
+
+
+            Console.WriteLine("\n\n╔════════════════════════════════╗");
+            Console.WriteLine("║         MENU ITEM HERE         ║");
+            Console.WriteLine("╚════════════════════════════════╝");
+            
+            Console.ReadLine();
+            Console.Clear();
+            #endregion                 
+
+        }
+    }
+
+
+}
+
